f5044d83ba08cffd4241f427586f51dcc78052fe
repo Destroyer <118625371+CryptoMakson@users.noreply.github.com>
2024-11-27 22:50:44 +0300

    tested Login.jsx

--- ArtTrack/src/components/Login.jsx
+++ ArtTrack/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import "../styles/basic.css"
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-
+ 
 export default function Login() {
     const [formData, setFormData] = useState({ username: '', password: '' });
     const navigate = useNavigate()
